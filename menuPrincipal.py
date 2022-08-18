f3eab7a42cd39859acf24fecc87501297938b28d
Ruben Ralda <rubenralda@gmail.com>
2022-08-18 13:18:04 -0600

    otro archivo

--- menuPrincipal.py
+++ menuPrincipal.py
@@ -9,6 +9,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import cargarArchivo
 
 
 class Ui_Dialog(object):
@@ -18,7 +19,7 @@ class Ui_Dialog(object):
         self.textEdit = QtWidgets.QTextEdit(Dialog)
         self.textEdit.setGeometry(QtCore.QRect(10, 10, 361, 81))
         self.textEdit.setObjectName("textEdit")
-        self.buttonCargar = QtWidgets.QPushButton(Dialog,clicked = lambda: self.probando())
+        self.buttonCargar = QtWidgets.QPushButton(Dialog)
         self.buttonCargar.setGeometry(QtCore.QRect(130, 130, 121, 31))
         self.buttonCargar.setObjectName("buttonCargar")
         self.buttonCursos = QtWidgets.QPushButton(Dialog)
@@ -33,24 +34,27 @@ class Ui_Dialog(object):
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
-    def probando(self):
-        self.textEdit.setText("lo cambie todo")
+        self.buttonCargar.clicked.connect(self.abrirCargarArchivo)
+
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
         self.textEdit.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Nombre del Curso: Laboratorio Lenguajes Formales y de Programación</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Nombre del Estudiante: Rubén Alejandro Ralda Mejia</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Carné del Estudiante: 202111835</p></body></html>"))
+                                         "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                         "p, li { white-space: pre-wrap; }\n"
+                                         "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+                                         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Nombre del Curso: Laboratorio Lenguajes Formales y de Programación</p>\n"
+                                         "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+                                         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Nombre del Estudiante: Rubén Alejandro Ralda Mejia</p>\n"
+                                         "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+                                         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Carné del Estudiante: 202111835</p></body></html>"))
         self.buttonCargar.setText(_translate("Dialog", "Cargar Archivo"))
         self.buttonCursos.setText(_translate("Dialog", "Gestionar Cursos"))
         self.buttonCreditos.setText(_translate("Dialog", "Conteo de Créditos"))
         self.buttonSalir.setText(_translate("Dialog", "Salir"))
+
+    def abrirCargarArchivo(self):
+        self.textEdit.setText("lo cambie todo")
 
 
 if __name__ == "__main__":
